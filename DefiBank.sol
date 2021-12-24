8138aced5b8cb8b730a7999e4e5a31629a51cde0
Nazanin Goudarzi <31389568+nazaninGoudarzi@users.noreply.github.com>
2021-12-25 00:31:33 +0330

    Update DefiBank.sol

--- DefiBank.sol
+++ DefiBank.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity ^0.6.12;
 
 interface MyERC20 {
     // When this limit is reached, the smart contract will refuse to create new tokens
@@ -23,33 +23,88 @@ interface MyERC20 {
 
 contract DefiBank {
     // Declaration
-    address owner;
+    address Owner;
+    address public USDC;
+    address public MTToken;
+
     mapping (uint => customer) customersInfo;
     uint numOfCustomers = 0;
     struct customer {
         address account;
         uint accountBalance;
+        uint stakeTime;
     }
 
     // Initiating the owner of the DefiBank
-    constructor() {
-        owner = msg.sender;
-        customersInfo[numOfCustomers].account = owner;
-        customersInfo[numOfCustomers].accountBalance = MyERC20(owner).balanceOf(owner);
+    constructor() public{
+        USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+        MTToken = 0x2B0746E89Bc60bd196dAC88A095c7340e175D077;
+        Owner = msg.sender;
+        //customersInfo[numOfCustomers].account = Owner;
+        //customersInfo[numOfCustomers].accountBalance = MyERC20(USDC).totalSupply();
     }
 
     // You can use this function to stake tokens
     function stake () external payable {
+        // If we already have this customer, this will be true
+        bool temp = false;
+
+        //This is for old customers
         for (uint i = 0; i < numOfCustomers; i++) {
-            if()
+            if(customersInfo[i].account == msg.sender) {
+                MyERC20(USDC).transferFrom(msg.sender, address(this), msg.value);
+                customersInfo[i].accountBalance += msg.value;
+                temp = true;
+                break;
+            }
         }
-        MyERC20(owner).transferFrom(msg.sender,address(this),msg.value);
-        customersInfo[numOfCustomers].accountBalance += msg.value;
+        // This is for new customers
+        if(temp = false) {
+            MyERC20(USDC).transferFrom(msg.sender, address(this), msg.value);
+            customersInfo[numOfCustomers].account = msg.sender;
+            customersInfo[numOfCustomers].accountBalance = msg.value;
+            customersInfo[numOfCustomers].stakeTime = block.timestamp;
+            numOfCustomers ++;
+        }
     }
 
     // You can use this function to unstake tokens
     function unstake () public {
-        for (uint i = 0; i < numOfCustomers; i++) {}
+        // If we already have this customer, this will be true
+        bool temp = false;
+
+        for (uint i = 1; i < numOfCustomers; i++) {
+            if(customersInfo[i].account == msg.sender) {
+                uint totalBalance = customersInfo[i].accountBalance;
+                require(totalBalance > 0,'You have already received your tokens!!');
+                if(totalBalance > 0) {
+                    MyERC20(USDC).transfer(msg.sender, totalBalance);
+                    // We will remove the customer
+                    customersInfo[i].account = 0x0000000000000000000000000000000000000000;
+                    customersInfo[i].accountBalance = 0;
+                    temp = true;
+                    break;
+                }
+            }
+        }
+        if(!temp) {
+            require(temp,'You do not ave any token in Defi Bank!');
+        }
+    }
+
+    // This function is for interest payment
+    function interestPayment () public {
+        require (msg.sender == Owner,'You do not have permission to access this section.');
+        for (uint i = 0; i < numOfCustomers; i++) {
+            if(customersInfo[i].accountBalance != 0) {
+                uint currentTime = block.timestamp;
+                uint difference = (currentTime - (customersInfo[i].stakeTime)) / 60 / 60 / 24;
+                if(difference >= 30 days) {
+                    customersInfo[i].accountBalance += (5 * customersInfo[i].accountBalance) / 100;
+                    MyERC20(MTToken).transfer(customersInfo[i].account, customersInfo[i].accountBalance);
+                }
+            }
+        }
     }
 }
 
